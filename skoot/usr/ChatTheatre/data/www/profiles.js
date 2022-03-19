da3fdbfeb0ce4eb11679e4b1ef969e753b767293
Sarah Tackett <sarahtackett@gmail.com>
2022-03-19 11:04:15 -0400

    Changes gables references

--- skoot/usr/ChatTheatre/data/www/profiles.js
+++ skoot/usr/ChatTheatre/data/www/profiles.js
@@ -1,14 +1,14 @@
 "use strict";
 // orchil/profiles.js
 var profiles = {
-        "portal_chattheatre":{
+        "portal_client":{
                 "method":   "websocket",
                 "protocol": "ws",
                 "server":   "localhost", //"chat.gables.chattheatre.com",
                 "http_port": 11080,
                 "port":      11801,
                 "woe_port":  11802,
-                "path":     "/chattheatre",
+                "path":     "/client",
                 "extra":    "",
                 "reports":   false,
                 "chars":    true,
